95e14a21e969c6308c0b10209e25f77574c588e4
Luuk Veenis <luukveenis@shaw.ca>
2015-09-29 13:14:32 -0700

    Add City class

---
+++ city.rb
@@ -0,0 +1,21 @@
+class City
+  attr_reader :name, :coord
+
+  def initialize name, val, coord
+    @name = name
+    @val = val
+    @coord = coord
+  end
+
+  def distance_to other
+    @coord.distance_to(other.coord)
+  end
+
+  def closest_five cities
+    cities.sort_by { |city| city.distance_to(self) }.first(5)
+  end
+
+  def to_i
+    @val
+  end
+end
