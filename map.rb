f95b2c2e4d7ff56c6f4f088996dafbee7db3cbf9
Luuk Veenis <luukveenis@shaw.ca>
2015-09-29 13:14:51 -0700

    Implement map class
    This is to represent the entire problem. We can generate new random
    instances of the problem by calling Map.generate. This will random place
    26 cities on a 100 x 100 grid and connect them using the R2 method
    described in the problem description. That is, for each city it will
    find the 5 closest cities and connect it to 3 of those. The distance
    between the cities is the Euclidian distance between them based on their
    coordinates.

---
+++ map.rb
@@ -0,0 +1,51 @@
+require 'set'
+require 'pry'
+require './city'
+require './coordinate'
+
+class Map
+  attr_reader :cities, :adjacency
+
+  def initialize cities, adjacency
+    @cities = cities
+    @adjacency = adjacency
+  end
+
+  def self.generate
+    cities = generate_cities
+    Map.new(cities, generate_adjacencies(cities))
+  end
+
+  def to_s
+    @adjacency.map { |r| r = r.map{ |e| e.round.to_s }.join(" ") }.join("\n")
+  end
+
+  private
+
+  # Generates 26 cities at guaranteed unique locations
+  def self.generate_cities
+    coords = generate_coords
+    ("A".."Z").zip((0..25)).map do |pair|
+      City.new(pair[0], pair[1], coords.pop)
+    end
+  end
+
+  # Generates an array of unique coordinate pairs
+  def self.generate_coords
+    coords = ::Set.new
+    random = ::Random.new
+    until coords.size == 26 do
+      coords << [random.rand(100), random.rand(100)]
+    end
+    coords.map { |c| ::Coordinate.new(c[0], c[1]) }
+  end
+
+  def self.generate_adjacencies cities
+    matrix = Array.new(26) { Array.new(26, 0) }
+    cities.each do |city|
+      closest = city.closest_five(cities - [city]).sample(3)
+      closest.each { |c| matrix[city.to_i][c.to_i] = city.distance_to(c) }
+    end
+    matrix
+  end
+end
